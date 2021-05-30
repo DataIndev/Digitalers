b94a4ca849bcc081c41a3da8668495b0ff81c11c
Benjamin <admin@dataindev.com>
2021-05-29 21:38:08 -0300

    Update

--- Clase02/ej2.js
+++ Clase02/ej2.js
@@ -1,5 +1,8 @@
 'use strict'
 
 window.addEventListener("load", () => {
+    var cantidadTiempo, diaASegundos,horaASegundos,minutoASegundos,cantidadDias,cantidadHoras,cantidadMinutos,cantidadSegundos;
+    
+
   
 });
